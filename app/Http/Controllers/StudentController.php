0ac89177a2154960a984b1b341796d78089505d9
Mukti Raj Bhusal <muktirajbhusal.cgc@gmail.com>
2025-08-03 23:02:15 +0530

    EloquentORM crud

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,52 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function addData(){
-        DB::table('students')->insert([
-            [
-            'name'=>'tester',
-            'email'=>'tester@tester.com',
-            'age'=>20,
-            'date_of_birth'=>'2006-01-01',
-            'gender'=>'m'
-            ],
-            [
-            'name'=>'tester2',
-            'email'=>'tester2@tester.com',
-            'age'=>20,
-            'date_of_birth'=>'2006-01-01',
-            'gender'=>'m'
-            ]
-        ]);
-
-        return 'Added data Successfully';
+        $item = new Student();
+        $item->name = 'tester';
+        $item->email = 'tester@tester.com';
+        $item->age = 20;
+        $item->date_of_birth = '2006-01-01';
+        $item->gender = 'm';
+        $item->score = 80;
+        $item->save();
+        return ' Added Data Successfully';
     }
 
     public function getData(){
-        $items=DB::table('students')
-        // ->where('id', '>=', 50)
-        // ->get();
-        // ->min('score');
-        ->avg('score');
-
+        $items = Student::all();
         return $items;
     }
 
     public function updateData(){
-        DB::table('students')->where('id', 101)->update([
-            'name'=>'updated Name',
-            'email'=>'updated@email.com'
-        ]);
-        return 'updated Data Successfully';
+        $item = Student::find(56);
+        $item->name = 'tester update';
+        $item->update();
+        return 'Updated Data Successfully';
     }
 
     public function deleteData(){
-        DB::table('students')->where('id', 101)->delete();
-        return 'deleted Data Successfully';
+        $item = Student::findOrFail(56);
+        $item->delete();
+        return "Deleted Data Successfully";
     }
 }
